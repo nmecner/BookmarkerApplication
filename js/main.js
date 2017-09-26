23d3419212ecd1bd158e7efe0a97106eaef978b9
Natalia Mecner <natalia.mecner@gmail.com>
2017-09-26 23:04:40 +0200

    added LocalStorage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
-//Listen for form submit
+
 
 var currentSubmit = document.getElementById("n-button-submit");
-var form = document.getElementById('add-site-form');
+var form = document.getElementById("add-site-form");
 
-function saveBookmark(){
-    console.log('It Works');
-};
 
-//Listen for the events
-//currentSubmit.addEventListener("click", function() {alert("clicked!")});
-form.addEventListener("submit", saveBookmark );
+
+function saveBookmark(e){
+    var siteName = document.getElementById("siteName").value;
+    var siteUrl = document.getElementById("siteUrl").value;
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    };
+
+
+    // check if bookmarks is null
+   if (localStorage.getItem("bookmarks") === null){
+
+     var bookmarks = [];
+     bookmarks.push(bookmark);
+     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+   } else {
+       var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+       bookmarks.push(bookmark);
+       localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+   }
+    
+
+    e.preventDefault();
+
+}
+
+
+form.addEventListener("submit", saveBookmark);
