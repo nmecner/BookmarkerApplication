ccbe902f505c5e3e3fa55fff44e0644f029725b7
Natalia Mecner <natalia.mecner@gmail.com>
2017-09-27 00:18:41 +0200

    fully working version without validation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 var currentSubmit = document.getElementById("n-button-submit");
 var form = document.getElementById("add-site-form");
+var results = document.getElementById("bookmarkResults");
 
 
 
@@ -23,13 +24,54 @@ function saveBookmark(e){
    } else {
        var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
        bookmarks.push(bookmark);
-       localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+       localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
    }
-    
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+    fetchBookmarks()
 
     e.preventDefault();
 
 }
+
+
+
+function deleteBookmark (url) {
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+    for (var i in bookmarks){
+        var bookmark_url = bookmarks[i].url;
+
+        if (url === bookmark_url){
+            bookmarks.splice(i, 1);
+        }
+    }
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+    fetchBookmarks()
+}
+
+
+function  fetchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    results.innerHTML = "";
+
+    //  for (var i = 0; i < bookmarks.length; i++) {
+
+    for (var i in bookmarks) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        results.innerHTML += '<div class="well">' +
+            '<h3>' + name +
+            ' <a class="btn btn-default" target="_blank" href="' + url + '">Visit</a> ' +
+            ' <a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">Delete</a> ' +
+            '</h3>' +
+            '</div>';
+    }
+}
+
+
 
 
 form.addEventListener("submit", saveBookmark);
